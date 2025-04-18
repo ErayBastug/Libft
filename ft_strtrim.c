69fba9c8698259a9435b10f60fe5f77501d208cb
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:13:00 +0300

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,45 +1,50 @@
-int ft_stristr(const char *haystack, const char *needle)
-{
-    int    i;
-    int    j;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:07:54 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 06:45:54 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    j = 0;
-    while (haystack[i] != '\0')
-    {
-        while (haystack[i + j] == needle[j])
-        {
-            if (needle[j + 1] == '\0')
-                return (i);
-            j++;
-        }
-        i++;
-    }
+#include "libft.h"
+#include <sys/_types/_null.h>
+
+static int	ft_chrchr(char *s, char c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    char *str;
-    int i;
-    int a;
+	unsigned int	i;
+	unsigned long	a;
+	int				c;
 
-    i = 0;
-    a = ft_stristr(s1, set);
-    str = (char *)malloc((strlen(s1) - strlen(set)) *sizeof(char) + 1);
-    if (str == NULL || s1[0] == NULL)
-        return (NULL);
-    while (i < a)
-    {
-        str[i] =  s1[i];
-        i++;
-    }
-    a = i + strlen(set);
-    while (s1[a])
-    {
-        str[i] =  s1[a];
-        i++;
-        a++;
-    }
-    str[i] = '\0';
-    return (str);
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	if (*s1 == '\0')
+		return (ft_strdup(""));
+	i = 0;
+	a = ft_strlen(s1) - 1;
+	while (ft_chrchr((char *)set, s1[i]))
+		i++;
+	while (ft_chrchr((char *)set, s1[a]))
+		a--;
+	c = a - (i - 1);
+	return (ft_substr(s1, i, c));
 }
