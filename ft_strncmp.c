39e6051ed3e04f2074d48abda3a8ebac366c9f6e
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:12:15 +0300

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,14 +1,33 @@
-int ft_strncmp(const char *s1, const char *s2, size_t n){
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 01:57:35 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 02:32:20 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while (i < n && (s1[i] || s2[i]))
-    {
-        if (s1[i] != s2[i])
-        {
-            return (s2[i] - s1[i]);
-        }
-        i++;
-    }
-    return (s2[i] - s1[i]);
+#include <sys/_types/_size_t.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (i < n && (str1[i] || str2[i]))
+	{
+		if (str1[i] != str2[i])
+		{
+			return (str1[i] - str2[i]);
+		}
+		i++;
+	}
+	return (0);
 }
