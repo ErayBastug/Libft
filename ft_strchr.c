43f96d5ed54643dc656541ea878b1d8683578140
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:08:51 +0300

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,26 +1,31 @@
-char *ft_strchr(const char *s, int c){
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 16:07:46 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 01:42:46 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    if (c == 0)
-    {
-        return ((char *)&s[i]);
-    }
-    while(s[i])
-    {
-        if(s[i] == c)
-        {
-            return ((char *)&s[i]);
-        }
-        i++;
-        
-    }
-    return NULL;
-}
+#include <sys/_types/_null.h>
 
-int main() 
+char	*ft_strchr(const char *s, int c)
 {
-    char s[] = "eray" ;
-   printf("%s\n", ft_strchr(s, b));
-    printf("%s", &s[2]);
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+	{
+		return ((char *)&s[i]);
+	}
+	return (NULL);
 }
