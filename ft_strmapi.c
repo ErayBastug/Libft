e7c64866a7b5bd3596c239ba0f9d557cc43430fb
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:12:03 +0300

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,17 +1,32 @@
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:09:53 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 09:24:27 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    char *str;
-    unsigned int i;
-    
-    i = 0;
-    str = (char *)malloc(ft_strlen(s) * sizeof(char) + 1);
-    if (str == NULL)
-        return (NULL);
-    while (s[i])
-    {
-        str[i] = f(i, s[i]);
-        i++;
-    }
-    str[i] = 0;
-    return (str);
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	str = (char *)malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
 }
