439258c27da0ff388566ad7e67862cf4e4b86eb6
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:11:22 +0300

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,39 +1,36 @@
-#include <stddef.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 17:05:04 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 01:43:16 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <sys/_types/_null.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t a;
-    size_t b;
-    size_t i;
+	size_t	a;
+	size_t	b;
+	size_t	i;
 
-    a = strlen(dst);
-    b = strlen(src);
-    i = 0;
-    if (dstsize >= a + b + 1)
-    {
-        while (src[i])
-        {
-            dst[a + i] = src[i];
-            i++;        
-        }    
-    }
-    else if (dstsize > a)
-    {
-        while (dstsize - a - i -1  > 0)
-        {
-            dst[a + i] = src[i];
-            i++;
-        }
-    }
-    dst[a + i] = '\0';
-    return (a + b);
-}
-#include <stdio.h>
-int main()
-{
-    char dst[] = "eray";
-    char src[] = "bastug";
-    printf("%zu\n", ft_strlcat(dst, src, 11));
-    printf("%s\n", dst);
+	a = ft_strlen(dst);
+	b = ft_strlen(src);
+	i = 0;
+	if (src == NULL || dst == NULL)
+		return (0);
+	if (dstsize <= a)
+		return (b + dstsize);
+	while (a + i < dstsize - 1 && src[i])
+	{
+		dst[a + i] = src[i];
+		i++;
+	}
+	dst[a + i] = '\0';
+	return (a + b);
 }
