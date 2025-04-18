36872148dd17d617eec019f045053f6beb289a9c
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:06:22 +0300

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,41 +1,81 @@
-int    ft_number_size(int number)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 20:09:51 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 01:43:49 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int	ft_number_size(int number)
 {
-    int    length;
-    
-    length = 0;
-    if (number == 0)
-        return (1);
-    if (number < 0)
-        length += 1;
-    while (number != 0)
-    {
-        number /= 10;
-        length++;
-    }
-    return (length);
+	int	length;
+
+	length = 0;
+	if (number == 0)
+		return (1);
+	if (number < 0)
+		length += 1;
+	while (number != 0)
+	{
+		number /= 10;
+		length++;
+	}
+	return (length);
 }
 
-char *ft_itoa(int n)
+static char	*inception(void)
 {
-    char *str;
-    int i;
-    int a;
-    
-    a = ft_number_size(n);
-    i = ft_number_size(n);
-    str = (char *)malloc(sizeof(char) * a + 1);
-    if (n < 0)
+	char	*str;
+
+	str = malloc(12);
+	if (!str)
+		return (NULL);
+	str[0] = '-';
+	str[1] = '2';
+	str[2] = '1';
+	str[3] = '4';
+	str[4] = '7';
+	str[5] = '4';
+	str[6] = '8';
+	str[7] = '3';
+	str[8] = '6';
+	str[9] = '4';
+	str[10] = '8';
+	str[11] = '\0';
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	int		i;
+	int		a;
+	char	*str;
+
+	a = ft_number_size(n);
+	i = ft_number_size(n);
+	if (n == -2147483648)
+		return (inception());
+	str = (char *)malloc(a + 1);
+	if (!str)
+		return (NULL);
+	if (n < 0)
 	{
 		str[0] = '-';
 		n = -n;
 	}
-    while (n != 0)
-    {
-        str[a - 1] = (n % 10) + 48;
-        n = n /10;
-        a--;
-    }
-    
-    str[i] = 0;
-    return(str);
+	if (n == 0)
+		str[0] = '0';
+	while (n != 0)
+	{
+		str[a-- - 1] = (n % 10) + 48;
+		n = n / 10;
+	}
+	str[i] = '\0';
+	return (str);
 }
