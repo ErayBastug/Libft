0172f85297223ae5ee0ec2e27c035c70f05bcd02
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:06:52 +0300

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,28 +1,31 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 16:30:14 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 02:24:53 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_memcmp(const void *s1, const void *s2, int n){
-    int i;
-    unsigned char *s3;
-    unsigned char *s4;
-    
-    s3 = (unsigned char *)s1;
-    s4 = (unsigned char *)s2;
-    
-    i = 0;
-    while (i < n && (s3[i] || s4[i]))
-    {
-        if (s3[i] != s4[i])
-        {
-            return (s3[i] - s4[i]);
-        }
-        i++;
-    }
-    return (0);
-}
+#include <sys/_types/_size_t.h>
 
-int main() 
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    char s1[] = "araya" ;
-    char s2[] = "bray" ;
-   printf("%d\n", ft_memcmp(s1, s2, '4'));
+	size_t			i;
+	unsigned char	*s3;
+	unsigned char	*s4;
+
+	s3 = (unsigned char *)s1;
+	s4 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (s3[i] != s4[i])
+			return ((s3[i] - s4[i]));
+		i++;
+	}
+	return (0);
 }
