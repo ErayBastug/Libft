e224a2b8ee12532ab3f3a4bb6e0844573142d6a7
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:12:48 +0300

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,18 +1,29 @@
-char *ft_strrchr(const char *s, int c){
-  int a;
-    
-  a = ft_strlen(s);
-  if (c == 0)
-  {
-      return &s[a];
-  }
-  while(a >= 0)
-  {
-    if(s[a] == c)
-    {
-      return &s[a];
-    }
-    a--;    
-  }
-    return NULL;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:01:52 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 06:26:41 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/_types/_null.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	a;
+
+	a = 0;
+	while (s[a])
+		a++;
+	while (a >= 0)
+	{
+		if (s[a] == (char)c)
+			return ((char *)&s[a]);
+		a--;
+	}
+	return (NULL);
 }
