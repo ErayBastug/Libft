0e21b926927e1b1737687653ad94c3d46a4b7522
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:03:42 +0300

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 01:46:23 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 02:25:58 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_atoi(const char *str)
 {
 	int	i;
@@ -7,7 +19,7 @@ int	ft_atoi(const char *str)
 	i = 0;
 	a = 1;
 	result = 0;
-	while (str[i] >= 9 && str[i] <= 13 || str[i] == 32)
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
@@ -24,10 +36,3 @@ int	ft_atoi(const char *str)
 	result *= a;
 	return (result);
 }
-
-// #include <stdio.h>
-
-// int main()
-// {
-// 	printf("%d", ft_atoi("-1234"));
-// }
