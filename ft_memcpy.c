dd44ea4e004dd676db42f840282a4f7b5a1e30df
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:07:05 +0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,12 +1,29 @@
-void	*ft_memcpy(void *dst, const void *src, int n)
-{
-	int	i;
-	char	*d;
-	char	*s;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:21:47 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 03:52:51 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include <sys/_types/_null.h>
+#include <sys/_types/_size_t.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*d;
+	unsigned char	*s;
+
+	if (dst == NULL && src == NULL)
+		return (0);
 	i = 0;
-	d = (char *)dst;
-	s = (char *)src;
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
 	while (i < n)
 	{
 		d[i] = s[i];
