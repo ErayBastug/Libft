767e3376a5616b30c6feb9898a805a4e032395e2
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:12:33 +0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,21 +1,36 @@
-char	*ft_strnstr(const char *haystack, const char *needle, size_t c)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 03:14:32 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 03:14:34 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/_types/_size_t.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int	i;
-	int	a;
+	size_t	i;
+	size_t	a;
 
 	i = 0;
-	a = 0;
-	if (*needle == 0)
+	if (*needle == '\0')
 		return ((char *)haystack);
-	while (i < c && haystack[i])
+	while (i < len && haystack[i])
 	{
+		a = 0;
 		if (haystack[i] == needle[a])
 		{
-			while (haystack[i + a] == needle[a])
+			while (haystack[i + a] == needle[a] && (i + a) < len)
+			{
 				a++;
-			if (needle[a] == 0 || needle[a - 1] == 0
-				&& haystack[i + a -1] == 0)
-				return ((char *)&haystack[i]);
+				if (needle[a] == '\0')
+					return ((char *)&haystack[i]);
+			}
 		}
 		i++;
 	}
