1820eced2c6fed613e7a6d7b203a5b9f482cf426
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:06:40 +0300

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,27 +1,30 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:21:42 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 08:36:21 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void *ft_memchr(const void *s, int c, int n){
-    int i;
+#include <sys/_types/_null.h>
+#include <sys/_types/_size_t.h>
 
-    i = 0;
-    if (c == 0)
-    {
-        return ((char *)&s[i]);
-    }
-    while(i < n && ((char *)s)[i] != 0)
-    {
-        if(((char *)s)[i] == c)
-        {
-            return ((char *)&s[i]);
-        }
-        i++;
-    }
-    return NULL;
-}
-
-int main() 
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-    char s[] = "eray" ;
-   printf("%s\n", ft_memchr(s, 'a', 4));
-    printf("%s", &s[2]);
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+		{
+			return ((void *)&s[i]);
+		}
+		i++;
+	}
+	return (NULL);
 }
