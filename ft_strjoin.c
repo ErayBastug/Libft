7bf062d03e5ac7f2dad7ed8e1da5460123f62c67
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:11:07 +0300

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,24 +1,41 @@
-char *ft_strjoin(char const *s1, char const *s2)
-{
-    char *str;
-    int i;
-    int a;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:05:53 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 08:40:01 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    a = 0;
-    str = (char *)malloc((strlen(s1) + strlen(s2)) *sizeof(char) + 1);
-    if (str == NULL)
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	a;
+
+	i = 0;
+	a = 0;
+	if (!s1 || !s2)
 		return (NULL);
-    while (i < strlen(s1))
-    {
-        str[i] = s1[i];
-        i++;
-    }
-    while (a < strlen(s2))
-    {
-        str[i + a] = s2[a];
-        a++;
-    }
-    str[i + a] = '\0';
-    return (str);
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (i < ft_strlen(s1))
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (a < ft_strlen(s2))
+	{
+		str[i + a] = s2[a];
+		a++;
+	}
+	str[i + a] = '\0';
+	return (str);
 }
