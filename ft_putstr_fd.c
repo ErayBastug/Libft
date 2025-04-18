2189223499b09218313e2e8c4c8245f952dff201
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:08:26 +0300

    Update ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,11 +1,26 @@
-void ft_putstr_fd(char *s, int fd)
-{
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:10:19 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 11:17:13 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while (i < strlen(s))
-    {
-        write(fd, &s[i], 1);
-        i++;
-    }
+#include "libft.h"
+#include <unistd.h>
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < ft_strlen(s))
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
 }
