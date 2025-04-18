61d2fd1bda78d11fb984249a35bdb475b06bef57
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:04:17 +0300

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,12 +1,26 @@
-void	*ft_calloc(int count, int size)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 01:54:11 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 03:20:27 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_calloc(size_t count, size_t size)
 {
-	void	*str;
-	int	i;
+	char	*str;
+	size_t	i;
 
 	i = 0;
-	if (str == NULL)
-    	return (NULL);
 	str = malloc(count * size);
+	if (str == NULL)
+		return (NULL);
 	while (i < count * size)
 	{
 		str[i] = 0;
