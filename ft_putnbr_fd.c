1a9eb01e587af26715c53d592c5dad7e89d641b2
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:08:14 +0300

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,26 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:24:32 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 11:18:47 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-    char a;
-    
-    if (n == -2147483648)
-        write(fd, &"-2147483648", 11);
-    else if (n < 0)
-    {
-        write(fd, "-", 1);
-        n = n * -1;
-        ft_putnbr_fd(n,fd);
-    }
-    else if (n > 9)
-    {
-        ft_putnbr_fd(n / 10, fd);
-        a = (n % 10) + '0';
-        write(fd, &a, 1);
-    }
-    else
-    {
-        a = n + 48;
-        write(fd, &a, 1);
-    }
+	char	a;
+
+	if (n == -2147483648)
+		write(fd, &"-2147483648", 11);
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = n * -1;
+		ft_putnbr_fd(n, fd);
+	}
+	else if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		a = (n % 10) + '0';
+		write(fd, &a, 1);
+	}
+	else
+	{
+		a = n + 48;
+		write(fd, &a, 1);
+	}
 }
