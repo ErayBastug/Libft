bf1af8c5709e2b337f2bc02a8492a5616ca66ecd
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:13:12 +0300

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -1,16 +1,36 @@
-char *ft_substr(char const *s, unsigned int start,
-size_t len)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:04:16 by erbastug          #+#    #+#             */
+/*   Updated: 2024/11/15 03:02:26 by erbastug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char *str;
-    int i;
-    
-    i = 0;
-    str = malloc(len * sizeof(char) + 1);
-    while (i < len)
-    {
-        str[i] = s[start + i];
-        i++;
-    }
-    str[i] = '\0';
-    return (str);
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (!s || start > ft_strlen(s))
+		return (ft_calloc(1, 1));
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	str = malloc(len * sizeof(char) + 1);
+	if (!str)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
